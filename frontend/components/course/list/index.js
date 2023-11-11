6e88f4229682d3a003b2d78255bd3c2e55ea6f53
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-11-12 01:58:16 +0400

    Adding pagination to application and also solving responsive problem on page

--- frontend/components/course/list/index.js
+++ frontend/components/course/list/index.js
@@ -1,9 +1,9 @@
-//!Course
-
 "use client"
 
-//!React
+//!React and Next
 import { useEffect, useState } from "react"
+import Image from "next/legacy/image"
+import Link from 'next/link'
 
 //!Third party packages
 import ReactPaginate from 'react-paginate';
@@ -11,39 +11,70 @@ import ReactPaginate from 'react-paginate';
 //!Pagination classess
 import '././../../../styles/pagination.css'
 
-export default function List(){
-    const [courses,setCourse] = useState([])
 
-    useEffect(()=>{
-        const getAllCourses = async () => {
-          fetch(`${process.env.api_root_endpoint}/courses/`)
+
+//*List
+export default function List(){
+    //state
+    const [courses,setCourse] = useState([])
+    const [itemOffset, setItemOffset] = useState(0);
+
+
+    //pagination
+    let itemsPerPage = 4
+    const endOffset = itemOffset + itemsPerPage;
+    const current_courses = courses.slice(itemOffset, endOffset)
+    const pageCount = Math.ceil(courses.length / itemsPerPage);
+
+
+    //getAllCourses
+    const getAllCourses = async () => {
+        fetch(`${process.env.api_root_endpoint}/courses/`)
             .then((response) => response.json())
             .then((data) => {
-              setCourse(data)
-            })
-        }
+                setCourse(data)
+        })
+    }
+
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % courses.length;
+        setItemOffset(newOffset);
+    };
+
+    useEffect(()=>{
         getAllCourses()
     },[])
 
 
-  return(
-        <section className="grid grid-cols-2 gap-4 mb-5">
-            { courses.map((course, index) => 
-              <div key={index} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-                  <div className="md:flex">
-                      <div className="md:flex-shrink-0">
-                          <img className="h-48 w-full object-cover md:w-48" src={course.cover_image} alt="Man looking at item at a store" />
-                      </div>
-                      <div className="p-8">
-                          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{course.course_type}</div>
-                          <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{course.title}</a>
-                          <p className="mt-2 text-gray-500">{course.description}</p>
-                      </div>
-                  </div>
-              </div>
-            )}
-
-
-        </section>
+    return(
+        <>
+            <section className="grid md:grid-cols-1 lg:grid-cols-2 gap-4 mb-5">
+                { current_courses.map((course, index) => 
+                    <div key={index} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                        <div className="flex h-full w-full">
+                            <div className="flex h-full w-full">
+                                <Image className="object-cover" src={course.cover_image} layout="fixed" width="200" height="230" alt={course.title} />
+                            </div>
+                            <div className="p-8">
+                                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{course.course_type}</div>
+                                <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{course.title}</Link>
+                                <p className="mt-2 text-gray-500">{course.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </section>
+            <ReactPaginate
+                pageCount={pageCount} 
+                pageRangeDisplayed={5} 
+                marginPagesDisplayed={2} 
+                onPageChange={handlePageClick} 
+                containerClassName={'pagination'} 
+                activeClassName={'active'} 
+                disabled={true}
+            />
+        </>
     )
 }

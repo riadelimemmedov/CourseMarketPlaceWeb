9f6e5f506e3b0a9b977519a4735c2feaca9331a2
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-12-01 20:17:54 +0400

    Updating notification handdling if user not encounter problem related to metamask

--- frontend/utils/notification.js
+++ frontend/utils/notification.js
@@ -1,11 +1,15 @@
+//!Third party packages
 import { toast } from 'react-toastify'
 
 
 //throwNotification
-const throwNotification = (notification_message=null,notification_type=null) => {
-    notification_type === 'success' ? toast.success(`${notification_message}`) : notification_type === 'error' ? toast.error(`${notification_message}`) : notification_type === 'info' ? toast.info(`${notification_message}`) : null;
+const throwNotification = (notification_message=null,notification_type=null,notification_code=null) => {
+    if(notification_code == "-32002"){
+        notification_type === 'success' ? toast.success(`${notification_message}`) : notification_type === 'error' ? toast.error(`${notification_message}`) : notification_type === 'info' ? toast.info(`${notification_message}`) : null;
+    }
+    else if(notification_code == null){
+        notification_type === 'success' ? toast.success(`${notification_message}`) : notification_type === 'error' ? toast.error(`${notification_message}`) : notification_type === 'info' ? toast.info(`${notification_message}`) : null;
+    }
 }
 
-export default {
-    throwNotification
-}
+export default {throwNotification}

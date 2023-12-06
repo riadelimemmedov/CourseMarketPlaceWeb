280d769eb4a297e4c523a8d5d74ebc40ab59f2f3
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-12-06 21:41:37 +0400

    Creating some new page

--- frontend/src/app/marketplace/page.js
+++ frontend/src/app/marketplace/page.js
@@ -6,6 +6,7 @@ import { Ethrate,Wallet } from '@components/ui/web3'
 import { CourseList } from '@components/ui/course'
 import { OrderCard, OrderModal } from '@components/ui/order'
 import { BaseLayout } from '@components/ui/layout'
+import { Header } from '@components/ui/marketplace'
 
 
 //!Helpers functions and methods
@@ -16,18 +17,12 @@ import { useWalletInfo } from '@components/providers/web3/hooks/useWalletInfo'
 
 //*Home
 export default function Marketplace({course}) {
-    const {account,network,isCanPurchaseCourse} = useWalletInfo()
-    const { connect,isLoading,error_code,web3} = useWeb3()
-    const { eth } = useEthPrice()
-
-
     //return jsx to client
     return (
         <>
             <BaseLayout>
                 <div className="py-4">
-                    <Wallet address={account.data} network={network.data} web3={web3} isLoading={isLoading}/>
-                    <Ethrate eth={eth.data}/>
+                    <Header/>  
                 </div>
                 <CourseList/>
             </BaseLayout>

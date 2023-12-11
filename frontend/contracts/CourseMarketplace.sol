0e076ff8dc5cb1744efbf5b8337481c9c78df34e
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-12-11 22:40:18 +0400

    Writing first contract for course marketplace

---
+++ frontend/contracts/CourseMarketplace.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//!CourseMarketPlace
+contract CourseMarketPlace {
+    //State of buyed course
+    enum State {
+        Purchased,
+        Activated,
+        Deactivated
+    }
+
+    //Course object
+    struct Course {
+        uint id;
+        uint price;
+        bytes32 proof;
+        address owner;
+        State state;
+    }
+
+    //mapping of courseHash to Course data
+    mapping(bytes32 => Course) private ownedCourses;
+
+    //mapping of courseId to courseHash
+    mapping(uint => bytes32) private ownedCourseHash;
+
+    //Total number of course owned to current user
+    uint private totalOwnedCourse;
+
+    //purchase course from current user
+    function purchaseCourse(bytes16 courseId, bytes32 proof) external payable {
+        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+        uint id = totalOwnedCourse++;
+        ownedCourseHash[id] = courseHash;
+        ownedCourses[courseHash] = Course({
+            id: id,
+            price: msg.value,
+            proof: proof,
+            owner: msg.sender,
+            state: State.Purchased
+        });
+    }
+
+    //get number of course owned to current user
+    function getCourseCount() external view returns (uint) {
+        return totalOwnedCourse;
+    }
+
+    //get course match to hash index
+    function getCourseHashAtIndex(uint index) external view returns (bytes32) {
+        return ownedCourseHash[index];
+    }
+
+    //get course instance using coursehash
+    function getCourseByHash(
+        bytes32 courseHash
+    ) external view returns (Course memory) {
+        return ownedCourses[courseHash];
+    }
+}

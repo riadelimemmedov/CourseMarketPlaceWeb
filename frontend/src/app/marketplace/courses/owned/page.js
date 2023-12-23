d1a607e510eacfe3259c032afe7e1225209ba6e4
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-12-23 22:59:19 +0400

    Updating merketplace and owned page

--- frontend/src/app/marketplace/courses/owned/page.js
+++ frontend/src/app/marketplace/courses/owned/page.js
@@ -1,15 +1,41 @@
 "use client"
 
+//!React and Next.js
+import { useEffect, useState } from "react"
+
 //!Custom components
 import { BaseLayout } from "@components/ui/layout"
 import { Header } from "@components/ui/marketplace"
 import { OwnedCourseCard } from "@components/ui/course"
 import { Button, Message } from "@components/ui/common"
 
+//!Helpers methods
+import { useOwnedCourses } from "@components/hooks/web3/useOwnedCourses"
+import { useAccount } from "@components/hooks/web3/useAccount"
+
 
 
 //*OwnedCourses
 export default function OwnedCourses () {
+        const { account } = useAccount()
+        const [courses,setCourse] = useState([])
+
+        //getAllCourses
+        const get_all_courses = async () => {
+            fetch(`${process.env.api_root_endpoint}/courses/`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setCourse(data)
+            })
+        }
+
+        const ownedCourses = useOwnedCourses(courses,account)
+
+
+        useEffect(() => {
+            get_all_courses()
+        },[])
+
         //return jsx to client
         return (
             <>

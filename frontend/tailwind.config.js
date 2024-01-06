83cc840a380ec290fb8434d991ae35726dd0feda
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-07 00:27:23 +0400

    Improve course card components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      flex: {
+        "2": "2 2 0%",
+        "3": "3 3 0%",
+        "4": "4 4 0%"
+      },
       maxWidth:{
         "8xl": "1920px"
       },
